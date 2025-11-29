090e1f246f79e9a290b81bf666101d0c933ed18e
Yuvraj Karekar <yuvrajkarekar5@gamil.com>
2025-11-29 16:44:50 +0530

    added Login page

--- Frontend/Courier-Service-Management/src/App.jsx
+++ Frontend/Courier-Service-Management/src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Registration from './pages/Registration/Registration';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Registration from "./pages/Registration/Registration";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/register" element={<Registration />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </Router>
   );
