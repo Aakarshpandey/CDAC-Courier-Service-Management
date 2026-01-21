35852d6b69119e40ec14757c6acaf2de14edbb4d
Aakarsh <aakarshpandey8438@gmail.com>
2026-01-21 17:28:28 +0530

    Partnerer Entities Created

--- Backend/CourierKaro/src/main/java/com/cms/CourierKaro/entity/PartnerLocation.java
+++ Backend/CourierKaro/src/main/java/com/cms/CourierKaro/entity/PartnerLocation.java
@@ -1,5 +1,41 @@
 package com.cms.CourierKaro.entity;
 
-public class PartnerLocation {
+import java.sql.Timestamp;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="PARTNER_LOCATIONS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PartnerLocation {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Location locationId;
+	
+	@JoinColumn(name = "partner_id")
+	private Partner partner;
+	
+	private Double lat;
+	
+	private Double lng;
+	
+	@Column(name="is_online")
+	private boolean isOnline;
+	
+	private Timestamp timestamp;	
+	
 }
---
+++ Backend/CourierKaro/src/main/java/com/cms/CourierKaro/entity/Status.java
@@ -0,0 +1,5 @@
+package com.cms.CourierKaro.entity;
+
+public enum Status {
+	ACTIVE, INACTIVE, SUSPENDED, DELETED;
+}
