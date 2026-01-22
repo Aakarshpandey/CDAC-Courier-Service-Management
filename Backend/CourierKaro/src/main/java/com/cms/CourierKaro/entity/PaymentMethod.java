20da2ef78b6783d67010c0cb1099eb5047384291
Vipul Bagde <vipulbagde1897@gmail.com>
2026-01-22 14:58:48 +0530

    added entites payment, systemLog, rating

---
+++ Backend/CourierKaro/src/main/java/com/cms/CourierKaro/entity/PaymentMethod.java
@@ -0,0 +1,9 @@
+package com.cms.CourierKaro.entity;
+
+public enum PaymentMethod {
+	  UPI,
+	    CREDIT_CARD,
+	    DEBIT_CARD,
+	    NET_BANKING,
+	    CASH
+}
--- Backend/CourierKaro/src/main/java/com/cms/CourierKaro/entity/Rating.java
+++ Backend/CourierKaro/src/main/java/com/cms/CourierKaro/entity/Rating.java
@@ -1,5 +1,43 @@
 package com.cms.CourierKaro.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ratings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Rating {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ratingId;
+
+    @OneToOne
+    @JoinColumn(name = "shipment_id", nullable = false)
+    private Shipment shipmentId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
+
+    @ManyToOne
+    @JoinColumn(name = "partner_id", nullable = false)
+    private Partner partnerId;
+
+    @Column(name = "rating", nullable = false)
+    private int rating;
+
+    @Column(name = "review", columnDefinition = "TEXT")
+    private String review;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
