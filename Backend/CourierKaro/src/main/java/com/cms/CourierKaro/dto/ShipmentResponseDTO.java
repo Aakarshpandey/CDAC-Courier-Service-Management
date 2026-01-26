d27b98fc54136b92968655aeed72de075c9aa715
Vipul Bagde <vipulbagde1897@gmail.com>
2026-01-26 12:07:36 +0530

    created current orders, order tracking -vipul

--- Backend/CourierKaro/src/main/java/com/cms/CourierKaro/dto/ShipmentResponseDTO.java
+++ Backend/CourierKaro/src/main/java/com/cms/CourierKaro/dto/ShipmentResponseDTO.java
@@ -3,6 +3,9 @@ package com.cms.CourierKaro.dto;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import com.cms.CourierKaro.entity.PackageType;
+import com.cms.CourierKaro.entity.Status;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -39,4 +42,45 @@ public class ShipmentResponseDTO {
     private BigDecimal calculatedPrice;
 
     private LocalDateTime createdAt;
+    
+    public ShipmentResponseDTO(
+            Long shipmentId,
+            Status status,
+            String pickupAddress,
+            String pickupContactName,
+            String pickupPhone,
+            String pickupPincode,
+            String deliveryAddress,
+            String deliveryContactName,
+            String deliveryPhone,
+            String deliveryPincode,
+            PackageType packageType,
+            BigDecimal weight,
+            String vehicleType,
+            BigDecimal baseFare,
+            BigDecimal perKmRate,
+            BigDecimal distanceKm,
+            BigDecimal calculatedPrice,
+            LocalDateTime createdAt
+    ) {
+        this.shipmentId = shipmentId;
+        this.status = status != null ? status.name() : "PENDING";
+        this.message = "Shipment retrieved successfully";
+        this.pickupAddress = pickupAddress;
+        this.pickupContactName = pickupContactName;
+        this.pickupPhone = pickupPhone;
+        this.pickupPincode = pickupPincode;
+        this.deliveryAddress = deliveryAddress;
+        this.deliveryContactName = deliveryContactName;
+        this.deliveryPhone = deliveryPhone;
+        this.deliveryPincode = deliveryPincode;
+        this.packageType = packageType != null ? packageType.name() : null;
+        this.weight = weight;
+        this.vehicleType = vehicleType;
+        this.baseFare = baseFare;
+        this.perKmRate = perKmRate;
+        this.distanceKm = distanceKm;
+        this.calculatedPrice = calculatedPrice;
+        this.createdAt = createdAt;
+    }
 }
--- Backend/CourierKaro/src/main/java/com/cms/CourierKaro/service/ShipmentService.java
+++ Backend/CourierKaro/src/main/java/com/cms/CourierKaro/service/ShipmentService.java
@@ -8,6 +8,7 @@ import com.cms.CourierKaro.dto.ShipmentResponseDTO;
 
 public interface ShipmentService {
     ShipmentResponseDTO createShipment(ShipmentRequestDTO request, String userEmail);
-
+    List<ShipmentResponseDTO> getUserShipments(String userEmail);
+    ShipmentResponseDTO getShipmentById(Long shipmentId);
 	List<RecentOrdersDTO> getShipments(String userEmail);
 }
